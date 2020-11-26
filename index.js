73d535cfb8ccc5700825e66e68d5563381a8f549
azerty <matthias.pellacoeur@hotmail.com>
2020-11-26 12:21:51 +0100

    search maps

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
-var crd
-var crdLatitude
-var crdLongitude
-function initMap() {
-    var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
-    
-    function success(pos) {
-        crd = pos.coords;
-        crdLatitude = crd.latitude
-        crdLongitude = crd.longitude
-        console.log(crd.latitude);
-        console.log(crd.longitude);
-        return {
-            crdLatitude: crdLatitude,
-            crdLongitude: crdLongitude
-            };
-    }
-    
-    function error(err){
-        console.warn(`ERREUR (${err.code}: ${err.message})`)
-    }
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(success, error, options)
-    }
-
-
-    const MyPosition = { lat: crdLatitude, lng: crdLongitude}
-    const RoanneDechetterie = { lat: 46.054250, lng: 4.050996 };
-    const LeCoteauDechetterie = { lat: 46.025519, lng: 4.082049 };
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initAutocomplete() {
     const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: MyPosition,
+      center: { lat: -33.8688, lng: 151.2195 },
+      zoom: 13,
+      mapTypeId: "roadmap",
     });
-    new google.maps.Marker({
-        position: RoanneDechetterie,
-        map,
-        title: "Roanne!",
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
     });
-    new google.maps.Marker({
-        position: LeCoteauDechetterie,
-        map,
-        title: "Le Coteau!",
+    let markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+  
+      if (places.length == 0) {
+        return;
+      }
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+      places.forEach((place) => {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            icon,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+  
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
     });
-}
+  }
